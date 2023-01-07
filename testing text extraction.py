c49263baa401ae3e610463c3c1e86795df383c15
umerindented <umerfiaz251@gmail.com>
2023-01-07 12:31:39 -0700

    "extracts ingredient data"

--- testing text extraction.py
+++ testing text extraction.py
@@ -1,1 +1,25 @@
 # test file
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url_test = input
+
+
+def extractdata(url):
+    r = requests.get(url)
+    return r.text
+
+
+htmldata = extractdata("https://www.simplyrecipes.com/recipes/homemade_pizza/")
+soup = BeautifulSoup(htmldata, 'html.parser')
+data = ''
+for data in soup.find_all("p"):
+    print(data.get_text())
+
+print("\n")
+print("\n")
+
+data1 = soup.find('ul')
+for li in data1.find_all("li"):
+    print(li.text, end=" ")
