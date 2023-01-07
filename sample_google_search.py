132688ee9651867e573935261c20dea307cf672e
Harrison Pang <112908220+pangstar019@users.noreply.github.com>
2023-01-07 16:40:10 -0700

    Add files via upload

--- sample_google_search.py
+++ sample_google_search.py
@@ -3,18 +3,26 @@ try:
 except ImportError:
     print(" 'google' not found")
 
-query = "low fat burritos" # we would need to concatentate user key words here
+while True:
+    prompt = input('Enter your search prompt: ').split( )
+    if len(prompt) > 5:
+        print('Enter a shorter search')
+    else:
+        break
+prompt = str(prompt)
+query = prompt + ' recipe'
 
 url_list = []
 
 while len(url_list) <= 10:
     for j in search(query, tld="com", num=20, stop=20, pause=0):
-        if 'youtube' in j or 'tiktok' in j:
+        if 'youtube' in j or 'tiktok' in j or 'pintrest' in j or 'facebook' in j or '.gov' in j:
             pass
         else:
             url_list.append(j)
             if len(url_list) >= 10:
                 break
 url_list.pop()
-print(url_list)
+for i in url_list:
+    print(i)
 
