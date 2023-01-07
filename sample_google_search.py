afaedc6be4871336c930ab4bacf6cef007bb79d7
umerindented <umerfiaz251@gmail.com>
2023-01-07 13:01:20 -0700

    "umer sample search"

--- sample_google_search.py
+++ sample_google_search.py
@@ -7,3 +7,5 @@ query = "low fat burritos"  # we would need to concatentate user key words here
 
 for j in search(query, tld="com", num=10, stop=10, pause=2):
     print(j)  # need to extract specific lengths
+
+n
