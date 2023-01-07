c1a3b90c52ed068b90f5b4ae1adea6287e17c991
umerindented <umerfiaz251@gmail.com>
2023-01-07 12:52:50 -0700

    "trying to remove youtube"

--- sample_google_search.py
+++ sample_google_search.py
@@ -3,7 +3,21 @@ try:
 except ImportError:
     print(" 'google' not found")
 
-query = "low fat burritos" # we would need to concatentate user key words here
- 
-for j in search(query, tld="com", num=10, stop=10, pause=2):
-    print(j) #need to extract specific lengths
+query = "low fat burritos"  # we would need to concatentate user key words here
+searchten = 10
+
+for j in search(query, tld="com", num=searchten, stop=10, pause=2):
+    print(j)  # need to extract specific lengths
+
+
+notten = True
+listrecipe = []
+
+while notten:
+    for i in search(query, tld="com", num=searchten, stop=None, pause=2):
+        if "www.youtube.com" in i:
+            searchten += 1
+        elif len(listrecipe) == 10:
+            notten = False
+        else:
+            listrecipe.append(i)
