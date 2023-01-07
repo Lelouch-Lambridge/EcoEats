bd4995a0a093776dcfa2b06af112916f792ca8a6
Harrison Pang <112908220+pangstar019@users.noreply.github.com>
2023-01-07 13:22:08 -0700

    Add files via upload

--- sample_google_search.py
+++ sample_google_search.py
@@ -3,9 +3,18 @@ try:
 except ImportError:
     print(" 'google' not found")
 
-query = "low fat burritos"  # we would need to concatentate user key words here
+query = "low fat burritos" # we would need to concatentate user key words here
 
-for j in search(query, tld="com", num=10, stop=10, pause=2):
-    print(j)  # need to extract specific lengths
+url_list = []
 
-n
+while len(url_list) <= 10:
+    for j in search(query, tld="com", num=20, stop=20, pause=0):
+        if 'youtube' in j or 'tiktok' in j:
+            pass
+        else:
+            url_list.append(j)
+            if len(url_list) >= 10:
+                break
+url_list.pop()
+print(url_list[0])
+
