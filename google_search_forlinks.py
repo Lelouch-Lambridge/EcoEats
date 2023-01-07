07ea4e527065479d431d1919a88084eef128730f
Ruchali Aery <112783914+ruchali1@users.noreply.github.com>
2023-01-07 12:06:30 -0700

    Update google_search_forlinks.py

--- google_search_forlinks.py
+++ google_search_forlinks.py
@@ -3,7 +3,8 @@ try:
 except ImportError:
     print(" 'google' not found")
 
-query = "low fat burritos"
+query = "low fat burritos" #sample keywords, would need to use concatentation of user entered keywords
+
  
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
     print(j)
