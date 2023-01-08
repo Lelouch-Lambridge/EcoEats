8f6adf62a271bb08870f253b8f272ab8dab7e553
Lelouch-Lambridge <82173691+Lelouch-Lambridge@users.noreply.github.com>
2023-01-08 07:04:10 -0700

    Update kivystuff.py

--- kivystuff.py
+++ kivystuff.py
@@ -10,26 +10,44 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from googlesearch import search
 
-Builder.load_file('kivycrap.kv')
+Builder.load_file('kivystuff.kv')
 
 #where google search function would go
 def see(stuff):
- return ' '.join(stuff.split()[2:])
+ #return ' '.join(stuff.split()[2:]), '0'
+ 
+ try:
+  from googlesearch import search
+ except ImportError:
+  print(" 'google' not found")
+  prompt = str(' '.join(stuff.split()[2:]))
+ query = prompt + ' recipe'
+
+ url_list = []
+
+ while len(url_list) < 10:
+  for j in search(query, tld="com", num=20, stop=20, pause=0):
+   if not ('youtube' in j or 'tiktok' in j or 'pintrest' in j or 'facebook' in j or '.gov' in j):
+    url_list.append(j)
+ url_list.pop()
+ return url_list.pop(), 0
+ 
 
 class MyLayout(Widget):
  def press(self):
   words = self.ids.txt_in.text
-  #for once we get google search function running
-  #[exec(f"self.ids.re{n}.text, self.ids.c{n}.text = tuplist[{n}]") for n in range(0,len(tuplist))]
-  self.ids.re1.text = see(words)
-  #print(self.ids.re1)
-  #print(self.ids.re1)
-  #self.ids.c1 = ' '.join(words.split()[2:])
+  tuplist = see(words)
+  #while len(tuplist) > 5: tuplist.pop()
+  print(tuplist)
+  
+  #self.ids.re0.text = tuplist[0]
+  #self.ids.c0.text = tuplist[1]
+
+  #for n in range(len(tuplist)): exec(f"self.ids.re{n}.text, self.ids.c{n}.text = tuplist[{n}]") 
   self.ids.txt_in.text = 'Enter word: '
   return words
 
 class MyApp(App):
- def build(self):
-  return MyLayout()
+ def build(self): return MyLayout()
 
 MyApp().run()
