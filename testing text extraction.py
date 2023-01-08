7b7fadb4c7870b019e32d773ebea3c671a21c731
umerindented <umerfiaz251@gmail.com>
2023-01-08 09:18:22 -0700

    "commit finalized"

--- testing text extraction.py
+++ testing text extraction.py
@@ -1,20 +1,10 @@
 from parse_ingredients import parse_ingredient
-import re
-from collections import OrderedDict
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import lxml
-from urllib.request import Request, urlopen
 from recipe_scrapers import scrape_me
 from ingredient_parser import parse_ingredient
-import nltk
-import json
 from googlesearch import search
-import html2text
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 
 
 # stores url
@@ -36,7 +26,7 @@ listofinformation = []
 # add all info to listofinformation
 for i in range(0, len(final_list_of_ingredients)):
     listofinformation.append(parse_ingredient(final_list_of_ingredients[i]))
-print(listofinformation)
+# print(listofinformation)
 
 ingredientednames = []
 for j in range(0, len(listofinformation)):
@@ -49,24 +39,37 @@ for k in range(0, len(listofinformation)):
 unitforeach_ingredient = []
 for i in range(0, len(listofinformation)):
     unitforeach_ingredient.append(listofinformation[i]['unit'])
+print(listofinformation)
 
+unitdict = {'cups': 0.25, 'teaspoons': 0.005,
+            'tablespoons': 0.015, 'oz': 0.028, 'liters': 1, '': 1}
+carbon = 0
+for x in listofinformation:
+    query = x['name']
+    url_list = []
+    try:
+        from googlesearch import search
+    except ImportError:
+        print(" 'google' not found")
+    query = 'carbon footprint' + query
+    for j in search(query, tld="com", num=3, stop=3, pause=2):
+        url_list.append(j)
+    print(url_list)
+    url = url_list[0]
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    x = soup.find_all('h2', class_='_c639719a')[0:2]
+
+    elementofcarbonnumber = []
+    for points in x:
+        point = str(points.text)
+        elementofcarbonnumber.append(point)
+
+    foot = float(elementofcarbonnumber[0].replace('kg CO2e/kg', ''))
+    mod = unitdict[k] if (k := x['unit']) in unitdict.keys() else unitdict[j] if (
+        j := ''.join(k.split()[:-1])) in unitdict.keys() else 0
+    carbon += float(x['quantity'])*mod*foot
 
 # to search
-googleTrendsUrl = 'https://google.com'
-response = requests.get(googleTrendsUrl)
-if response.status_code == 200:
-    g_cookies = response.cookies.get_dict()
-
-url = 'https://apps.carboncloud.com/climatehub/agricultural-reports/benchmarks/7859e28f-2971-4f86-836e-616f56eac8c2'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-x = soup.find_all('h2', class_='_c639719a')[0:2]
-
-elementofcarbonnumber = []
-for points in x:
-    point = str(points.text)
-    elementofcarbonnumber.append(point)
-
-newcarbonvalues = []
-newcarbonvalues.append(elementofcarbonnumber[0].replace('kg CO2e/kg', ''))
-print(newcarbonvalues)
+#query= input('enter an ingredient name')
+    #url= "https://www.google.com/search?q=carbon+footprint+of " + query
